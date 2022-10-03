b3a0301701c91ac881b4264cb65b01f207f05c0f
Danteie <aleksandar.djuratovic93@gmail.com>
2022-10-03 13:51:49 +0200

    adding Card commponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Card from './Component/Card/Card'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+        <div className='countainer'>
+          <header>
+            <div>
+              <h1>Notification</h1>
+              <h1 className='number'>3</h1>
+            </div>
+            <h2 className='mark'>Mark all as read</h2>
+          </header>
+          <Card />
+        </div>
+    </main>
   );
 }
 
---
+++ src/Component/Card/Card.js
@@ -0,0 +1,17 @@
+import './Card.css'
+
+
+export default function Card() {
+  return (
+        <div className='card'>
+            <img alt='profil' />
+            <div>
+                <h2>text</h2>
+                <h2>time</h2>
+            </div>
+        </div>
+    
+  );
+}
+
+
