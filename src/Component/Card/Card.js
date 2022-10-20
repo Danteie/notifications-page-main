839ddbf63f51c689ea13eb3383493fe61cdbb32b
Danteie <aleksandar.djuratovic93@gmail.com>
2022-10-20 13:44:59 +0200

    adding gp-page

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,9 +1,11 @@
 import './Card.css'
-
+import { useState } from 'react';
 
 export default function Card({image,name,text,time,link,message,content,read}) {
 
-  
+  const [value, setValue] = useState(0); 
+
+
   function messages(message) {
     if (message) {
       return (<h2 className='message'>{message}</h2>)
@@ -16,13 +18,12 @@ export default function Card({image,name,text,time,link,message,content,read}) {
     }
   }
 
-  
-
+ 
   function isRead(read){
     if(!read){  
       return (
-        <div  className='card-read'>
-            <img alt='profile' src={image} />
+        <div  className='card-read' >
+            <img alt='profile' src={image}/>
             <div>
                 <h1 className='notification-read'><span className='nickname'>{name}</span> <span className='type-notification'>{text}</span> <span><a className='link' href='/'>{link}</a></span></h1>
                 <h1 className='time'>{time}</h1>
